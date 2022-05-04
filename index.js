b13d9659526f3458b79a8f6cdae79d21145cc339
Prashant Kumar Singh <prashantrkt2002@gmail.com>
2022-05-04 19:26:15 +0530

    Establish connection to database

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const express = require('express');
+const connectToMongo = require('./db');
+const express = require('express')
+connectToMongo();
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+
+//Available Routes
+
+
+//Listen for connections
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+   console.log(`Currently Listening at http://localhost:${port}`);
+});
