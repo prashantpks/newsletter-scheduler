bb741bf8da0e40d04d860e1023975135bdd546ba
Prashant Kumar Singh <prashantrkt2002@gmail.com>
2022-05-05 10:53:17 +0530

    Add nodemailer to send email without scheduling

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const connectToMongo = require('./db');
-const express = require('express')
+const express = require('express');
+const cron = require('node-cron');
+const nodemailer = require('nodemailer');
 connectToMongo();
 require('dotenv').config();
 
@@ -9,6 +11,34 @@ app.use(express.json());
 
 //Available Routes
 app.use('/',require('./routes/newsletter.route'));
+
+//Sending newsletters
+
+var transporter = nodemailer.createTransport({
+   host: 'smtp.gmail.com',
+   port: 587,
+   secure:false,
+   requireTLS:true,
+   auth: {
+     user: process.env.DEV_MAIL, // enter your email address
+     pass: process.env.DEV_MAIL_PASSWORD  // enter your visible/encripted password
+   }
+ });
+ 
+ var mailOptions = {
+   from: process.env.DEV_MAIL,
+   to: 'prashantr@gmail.com',
+   subject: "helklo",
+   text: "hkjhk"
+ };
+ 
+//  transporter.sendMail(mailOptions, function(error, info){
+//    if (error) {
+//      console.log(error);
+//    } else {
+//      console.log('Email was sent successfully: ' + info.response);
+//    }
+//  });
 
 
 //Listen for connections
